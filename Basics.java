ce4ded0e08e075b7b478c6e657b28e40a8bfefa1
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-01-24 12:43:07 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,13 @@
+CASTING
+
+public class TestWide {
+    public static void main(String args[]) {
+        String x= "hello" + '/' + "world";
+        char y='h';
+        int a='a' ;
+        long b=34 ;
+        float c=109 ;
+        double d=3.458D ;
+        System.out.println(x+"   "+a+"   "+b+"   "+c+"   "+d+"   "+y);
+    }
+}

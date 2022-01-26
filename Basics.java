dde5b484a6cc51770064b88e4409ea1e57d88864
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-01-26 17:57:54 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -211,4 +211,16 @@ public class Main {
 
 
 // Do-While Loop
+// The do/while loop is a variant of the while loop. 
+// This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
 
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            x = 1;
+           
+        } while (x<0);
+        System.out.println(x);
+    }
+}

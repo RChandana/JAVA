c4723d55e4973b9e544864be340b32646ac2acda
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-01-26 12:46:18 +0530

    Create JavaCodes.java

---
+++ JavaCodes.java
@@ -0,0 +1,17 @@
+//Print the largest number of the given three numbers.
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=140;
+	    int b=22;
+	    int c=10;
+	    if (a>b && a>c) {
+	        System.out.println(a);
+	    }
+	    else if (b>c){
+	        System.out.println(b);
+	    }
+	    else System.out.println(c);
+	}
+}

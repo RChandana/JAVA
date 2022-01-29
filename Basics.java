f0c6748425e2d789589e85b80128de04570d25b7
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-01-29 12:47:20 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -238,4 +238,18 @@ public class Main {
 }
 
 
-// 
+// Arrays
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+//To declare an array, define the variable type with square brackets:
+String[] items;
+
+// We have now declared a variable that holds an array of strings. 
+// To insert values to it, we can use an array literal - place the values in a comma-separated list, inside curly braces:
+
+String[] items = {"Table", "Chair", "Car", "Book"};
+
+// To access an array element refer to the index number.
+System.out.println(items[2]);
+
+
+

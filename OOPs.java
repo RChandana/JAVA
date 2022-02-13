12094cf1c1f545a8d9292e47627433ef3697ad38
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-13 19:42:03 +0530

    Update OOPs.java

--- OOPs.java
+++ OOPs.java
@@ -1,1 +1,24 @@
+/* A class is a template for an object, and an object is an instance of a class.
+ The data, or variables, defined within a class are called instance variables. The code is contained within methods. 
+ Collectively, the methods and variables defined within a class are called members of the class.
+ Variables defined within a class are called instance variables because each instance of the class (that is, each object of the class) contains its own copy of these variables. 
+ Thus, the data for one object is separate and unique from the data for another. 
+ The dot operator links the name of the object with the name of an instance variable. */
+
+// Finding the volume of the box.
+public class Main{
+    double width;
+    double height;
+    double depth;
+    public static void main(String args[]) {
+        Main mybox = new Main();
+        double vol;
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
+    
+        vol = mybox.width * mybox.height * mybox.depth;
+        System.out.println("Volume is " + vol);
+  }
+}
 

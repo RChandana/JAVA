4046b6a8fff2a37e8e4fc5a0a7d37736ce512a1e
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-16 10:39:36 +0530

    Update JavaCodes.java

--- JavaCodes.java
+++ JavaCodes.java
@@ -174,3 +174,23 @@ public class Main
     }  
   }
 }
+
+
+
+
+// Java program to add digits in a number
+
+import java.util.Scanner;  
+public class Main{  
+    public static void main(String arg[]){  
+        long number, sum;  
+        Scanner sc=new Scanner(System.in);  
+        System.out.print("Enter a number: ");  
+        number=sc.nextLong();  
+        for(sum=0; number!=0; number=number/10) {  
+            sum = sum + number % 10;  
+            
+        } 
+        System.out.println("Sum of digits: "+sum); 
+    }  
+}  

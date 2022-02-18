21f0d4dd4bd4d2905b28667ec38b77402a4425a0
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-18 13:18:56 +0530

    Update JavaCodes.java

--- JavaCodes.java
+++ JavaCodes.java
@@ -194,3 +194,128 @@ public class Main{
         System.out.println("Sum of digits: "+sum); 
     }  
 }  
+
+
+
+
+/*Create a class namely Student which should include the parameters name, register number and address. 
+Define instance methods for setting and retrieving values after that instantiate respective object too. 
+Save the application as lab2.java.*/
+
+import java.lang.*; public class Student{
+	public String name;
+        public String regNo;
+        public String address;
+        public Student (String stuName) { 
+		name = stuName;
+	}
+	public void setRegNo(String stuRegNo) {
+		regNo = stuRegNo; 
+	}
+	public void setAddress(String stuAddress) {
+		address = stuAddress;
+	}
+	public void main() {
+		System.out.println("Name: " + name); System.out.println("RegNo:" + regNo); System.out.println("Address:" + address);
+	}
+	public static void main(String args[]) {
+		Student StuOne = new Student("Chandana"); Student StuTwo = new Student("Sanjana"); Student StuThree = new Student("Shreya");
+		StuOne.setRegNo("21BCEXXXX"); StuTwo.setRegNo("21BCEXXXX"); StuThree.setRegNo("21BCEXXXX");
+		StuOne.setAddress("A"); StuTwo.setAddress("B"); StuThree.setAddress("C");
+		StuOne.main(); StuTwo.main(); StuThree.main();
+	}
+}
+
+
+
+
+/*Create a class named Billing that includes three overloaded computeBill() methods for a
+photo book store.
+
+When computeBill() receives a single parameter, it represents the price of one photo book ordered. Add 8% tax, and return the total due.
+
+When computeBill() receives two parameters, they represent the price of a photo book and the quantity ordered. 
+Multiply the two values, add 8% tax, and return the total due.
+
+When computeBill() receives three parameters, they represent the price of a photo book, the quantity ordered, and a coupon value. 
+Multiply the quantity and price, reduce the result by the coupon value, and then add 8% tax and return the total due.
+
+Write a main() method that tests all three overloaded methods. Save the application as Billing.java.*/
+
+
+public class Billing {
+	private float photobookprice=0; private int photobooks = 1;
+        private final int taxpercent = 8; private float couponValuePercent=0; private float totalDue=0;
+        public Billing(float photobookprice) { 
+		this.photobookprice = photobookprice; totalDue = calculateOneBookPrice();
+  }
+	public Billing(float photobookprice, int photobooks) { 
+		this(photobookprice);
+                this.photobooks = photobooks;
+                totalDue = calculateBooksPrice(); 
+	}
+        public Billing(float photobookprice, int photobooks, float couponValuePercent) {  
+		this(photobookprice, photobooks);
+                this.couponValuePercent = couponValuePercent;
+                totalDue = calculateBooksPriceWithCoupon();
+	}
+	private float calculateOneBookPrice() {
+		return photobookprice + (0.08f * photobookprice);
+	}
+	private float calculateBooksPrice() {
+		return calculateOneBookPrice() * photobooks;
+	}
+	private float calculateBooksPriceWithCoupon() {
+		return (calculateOneBookPrice() * photobooks) - couponValuePercent;
+	}
+	public static void main(String[] args) {
+		Billing bill1 = new Billing(250);
+		Billing bill2 = new Billing(250,4); Billing bill3 = new Billing(250,12,2.5f);
+		System.out.println(bill1.toString()); System.out.println(bill2.toString()); System.out.println(bill3.toString());
+	}
+	public float getTotalDue() { 
+		return totalDue;
+	}
+	public float getPhotobookprice() {
+		return photobookprice;
+	}
+	public void setPhotobookprice(float photobookprice) { 
+		this.photobookprice = photobookprice;
+	}
+	public int getPhotobooks() { 
+		return photobooks;
+	}
+	public void setPhotobooks(int photobooks) { 
+		this.photobooks = photobooks;
+	}
+	public float getCouponValuePercent() { 
+		return couponValuePercent;
+	}
+	public void setCouponValuePercent(float couponValuePercent) { 
+		this.couponValuePercent = couponValuePercent;
+	}
+	public int getTaxpercent() {
+		return taxpercent;
+	}
+	public String toString() {
+		return "PhotoBookPrice=" + photobookprice + "\n" + " PhotoBooks="+ photobooks + "\n" + "CouponValuePercent=" + couponValuePercent + "\n" + "TotalDue="+ totalDue ;
+} 
+}
+
+
+/* Write a Java application that uses the Math class to determine the answers for each of the following: (Use java.lang.Math class)
+a. The square root of 37
+b. The sine and cosine of 300
+c. The value of the floor, ceiling, and round of 22.8
+d. The larger and the smaller of the character ‘D’ and the integer 71
+e. A random number between 0 and 20
+(Hint: The random() method returns a value between 0 and 1; you want a number that is 20 times larger.) Save the application as MathTest.java.*/
+
+public class MathTest {
+	public static void main(String[] args) {
+        System.out.println("The square root of 37 = " + Math.sqrt(37)); System.out.println("The sine of 300 =" + Math.sin(300)); System.out.println("The cosine of 300 =" + Math.cos(300)); System.out.println("The value of the floor 22.8 =" + Math.floor(22.8)); System.out.println("The value of the ceiling 22.8 =" + Math.ceil(22.8)); System.out.println("The value of the round 22.8 =" + Math.round(22.8)); int number = 'D';
+        System.out.println("The larger of the character ‘D’ and the integer 71 =" + Math.max(number, 71));
+        System.out.println("The Smaller of the character ‘D’ and the integer 71 =" + Math.min(number, 71));
+        System.out.println("A random number between 0 and 20 = "+(int)(Math.random()* 20 + 1));
+	} 
+}

b4e32dda99495ac11fa441a3597f758ef8018f7f
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-02-25 12:26:49 +0530

    Update JavaCodes.java

--- JavaCodes.java
+++ JavaCodes.java
@@ -313,7 +313,12 @@ e. A random number between 0 and 20
 
 public class MathTest {
 	public static void main(String[] args) {
-        System.out.println("The square root of 37 = " + Math.sqrt(37)); System.out.println("The sine of 300 =" + Math.sin(300)); System.out.println("The cosine of 300 =" + Math.cos(300)); System.out.println("The value of the floor 22.8 =" + Math.floor(22.8)); System.out.println("The value of the ceiling 22.8 =" + Math.ceil(22.8)); System.out.println("The value of the round 22.8 =" + Math.round(22.8)); int number = 'D';
+        System.out.println("The square root of 37 = " + Math.sqrt(37)); 
+	System.out.println("The sine of 300 =" + Math.sin(300)); 
+	System.out.println("The cosine of 300 =" + Math.cos(300)); 
+	System.out.println("The value of the floor 22.8 =" + Math.floor(22.8)); 
+	System.out.println("The value of the ceiling 22.8 =" + Math.ceil(22.8));
+	System.out.println("The value of the round 22.8 =" + Math.round(22.8)); int number = 'D';
         System.out.println("The larger of the character ‘D’ and the integer 71 =" + Math.max(number, 71));
         System.out.println("The Smaller of the character ‘D’ and the integer 71 =" + Math.min(number, 71));
         System.out.println("A random number between 0 and 20 = "+(int)(Math.random()* 20 + 1));

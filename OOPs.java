73fd59c75408a7202095967310ea7449b88a0170
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-03-07 22:53:07 +0530

    Update OOPs.java

--- OOPs.java
+++ OOPs.java
@@ -96,4 +96,17 @@ class Bank
 	
 	
 	
-	
+// Garbage Collection 
+
+public class TestGarbage1{
+    public void finalize(){
+        System.out.println("object is garbage collected");
+    } 
+    public static void main(String args[]){
+        TestGarbage1 s1=new TestGarbage1(); 
+        TestGarbage1 s2=new TestGarbage1(); 
+        s1=null;
+        s2=null;
+        System.gc(); 
+    }
+}

f77377b2d067177abbd40e417b66e8959a22c5c2
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-03-12 00:02:59 +0530

    Update JavaCodes.java

--- JavaCodes.java
+++ JavaCodes.java
@@ -271,13 +271,7 @@ public class MathTest {
 
 
 
-/*Create  a  class  named Poem that  contains  fields  for  the  name  of  the  poem  and  the  number  of lines in it. 
-Include a constructor that requires values for both fields. Also include get methods to retrieve  field  values.  
-Create  three  subclasses: Couplet,  Limerick,  and  Haiku. 
-The  constructor for each subclass requires only a title; the lines field is set using a constant value.
-A couplet has two  lines,  a  limerick  has  five  lines,  and  a  haiku  has  three  lines.  
-Create  an  application  that demonstrates  usage  of  an  objectof  each  type. 
-Save  the  files  as  Poem.java,  Couplet.java, Limerick.java, Haiku.java, and DemoPoems.java.*/
+
 
 
 package demopoems;

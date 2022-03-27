32387543d9cfbfec959773b83f87a6d2226967d9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-03-27 12:04:55 +0530

    Update OPPs Questions.java

--- OPPs Questions.java
+++ OPPs Questions.java
@@ -603,3 +603,20 @@ public static void main(String[] args){
    multiplication(c1,c2);
 }
 }
+
+
+
+
+
+
+/* Create a class named College Course that includes data fields that hold
+the department (for example, CSE), the course number (for example, 110), the credits (for example,4), and the fee for the course (for example, Rs 360).  
+All of the fields are required as arguments to the constructor,  except  for  the  fee,  which  is  calculated  at  Rs 120  per  credit  hour.  
+Include  a display()  method  that  displays  the  course  data. Create  a  subclass  named  LabCourse  that adds  $50  to  the  course  fee.  
+Override the parent  class  display()  method  to  indicate  that  the course  is  a  lab  course  and  to  display  all  the  data. 
+Write  an  application  named  UseCourse that  prompts  the  user  for  course  information. 
+If  the  user  enters  a  class  in  any  of  the following departments,
+create a LabCourse: CSE, ECE, CIVIL, or IT. If the user enters any other  department,  create  a  CollegeCourse  that  does  not  include  the  lab  fee.  
+Then display the    course    data.    Save    the    files    as    CollegeCourse.java, LabCourse.java,    and UseCourse.java. */
+
+

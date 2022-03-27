9801d41d4843be8a21883ca145dcd0a6e84c62d5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-03-27 17:50:15 +0530

    Update OPPs Questions.java

--- OPPs Questions.java
+++ OPPs Questions.java
@@ -725,9 +725,9 @@ class Student{
         address = sadd;
     }
     public void showDetails(){
-        System.out.println(“ROll No :: ” + rollno);
-        System.out.println(“Name :: ” + name);
-        System.out.println(“Address :: ” + address);
+        System.out.println("Roll No : " + rollno);
+        System.out.println("Name : " + name);
+        System.out.println("Address : " + address);
     }
 }
 class Test extends Student{
@@ -742,10 +742,10 @@ class Test extends Student{
         marksSubject4 = m4;
     }
     public void showMarks(){
-        System.out.println(“Marks of Subjectl :: ” + marksSubjectl);
-        System.out.println(“Marks of Subject2 :: ” + marksSubject2);
-        System.out.println(“Marks of Subject3 :: ” + marksSubject3);
-        System.out.println(“Marks of Subject4 :: ” + marksSubject4);
+        System.out.println("Marks of Subjectl : " + marksSubjectl);
+        System.out.println("Marks of Subject2 : " + marksSubject2);
+        System.out.println("Marks of Subject3 : " + marksSubject3);
+        System.out.println("Marks of Subject4 : " + marksSubject4);
     }
 }
 class Result extends Test{
@@ -756,28 +756,36 @@ class Result extends Test{
         totalMarks = marksSubjectl + marksSubject2 + marksSubject3 + marksSubject4;
         percentage = (totalMarks*100.00F/600.00F);
         if (percentage >=50.00F)
-        grade=’D’;
+        grade = 'D';
         else
         if(percentage >=55.00F && percentage<=60.00F)
-        grade = ‘C’;
+        grade = 'C';
         else
         if (percentage >=61.00F && percentage<=70.00F)
-        grade = ‘B’;
+        grade = 'B';
         else
         if (percentage >=71.00F && percentage<=75.00F)
-        grade = ‘A’;
+        grade = 'A';
         else
         if (percentage >=76.00F && percentage<=85.00F)
-        grade = ‘H’;
+        grade = 'H';
         else
-        grade = ‘S’;
+        grade = 'S';
     }
     public void showResult(){
         showDetails();
         showMarks();
-        System.out.println(“Total Marks :: ” + totalMarks);
-        System.out.println(“percentage :: ” + percentage);
-        System.out.println(“Grade :: ” + grade);
+        System.out.println("Total Marks : " + totalMarks);
+        System.out.println("percentage : " + percentage);
+        System.out.println("Grade : " + grade);
     }
 }
-public class
+public class Multilevellnheritance{
+    public static void main(String ar[]){
+        Result ob = new Result();
+        ob.storeDetails(1001, "Alice", "New York");
+        ob.storeMarks(135,130,132,138);
+        ob.evaluateResult();
+        ob.showResult();
+    }
+}

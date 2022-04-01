9b3e5b324491858c355d19ed3576b032cf0d5804
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-04-01 12:15:19 +0530

    Update OPPs Questions.java

--- OPPs Questions.java
+++ OPPs Questions.java
@@ -780,3 +780,46 @@ class Multilevellnheritance{
         ob.showResult();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+class ComputeInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the principal: ");
+        double principal = input.nextDouble();
+        System.out.print("Enter the rate: ");
+        double rate = input.nextDouble();
+
+        System.out.print("Enter the time: ");
+        double time = input.nextDouble();
+        double simpleInterest = (principal * time * rate) / 100;
+
+        System.out.println("Principal: " + principal);
+        System.out.println("Interest Rate: " + rate);
+        System.out.println("Time Duration: " + time);
+        System.out.println("Simple Interest: " + simpleInterest);
+        
+        System.out.print("Enter number of times interest is compounded: ");
+        int number = input.nextInt();
+
+        double compoundInterest = principal * (Math.pow((1 + rate/100), (time * number))) - principal;
+
+        System.out.println("Principal: " + principal);
+        System.out.println("Interest Rate: " + rate);
+        System.out.println("Time Duration: " + time);
+        System.out.println("Number of times interest Compounded: " + number);
+        System.out.println("Compound Interest: " + compoundInterest);
+
+        input.close();
+  }
+}

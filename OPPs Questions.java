c42a63cdee18da74bb27189cf42321d86af4c8a5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-04-11 11:53:56 +0530

    Update OPPs Questions.java

--- OPPs Questions.java
+++ OPPs Questions.java
@@ -893,7 +893,20 @@ class Prof extends PHD_Scholar implements LabInCharge{
 
 
 
-//
+/*Implement the following.
+
+ a) Create  an  interface  named  Turner,  with  a  single  method  named  turn().
+    Create  a  class named  Leaf  that  implements  turn()  to  display  Changing  colors.  
+    Create  a  class  named Page  that  implements  turn( )  to  display  Going  to  the  next  page.  
+    Create  a  class  named Pancake  that  implements  turn()  to  display  Flipping.  
+    Write  an  application  named DemoTurners that creates one object of each of these class types and demonstrates the turn()  method  for  each  class.  
+    Save  the  files  as  Turner.java,  Leaf.java,  Page.java, Pancake.java, and DemoTurners.java.
+
+
+b) Think of two more objects that use turn(), create classes for them, and then add objects to the DemoTurners application, renaming it DemoTurners2. java. Save the files, using the names of new objects that use turn().
+
+
+c) Apply  Dynamic  method  dispatch  to  show  the  power  of  it  and  name  the  class  as DemoTurners3.java.*/
 
 import java.util.*;
 import java.lang.*;

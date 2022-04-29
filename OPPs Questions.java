2727e583356b53d0886629b198f9e6502f7a7d21
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-04-29 12:06:15 +0530

    Update OPPs Questions.java

--- OPPs Questions.java
+++ OPPs Questions.java
@@ -1059,19 +1059,57 @@ class BinaryCheck {
 
 
 
-
-package com.arssoft.numbers.util; public class Armstrong { public static boolean armstrong(int n){ int temp, digits=0, last=0, sum=0; temp=n; while(temp>0){ temp = temp/10; digits++; } temp = n; while(temp>0){ last = temp % 10; sum +=
-	
-	
-	
-	
-	}
-    class Amstrong {
+package com.arssoft.numbers.util;
+import java.util.Scanner;  
+import java.lang.Math;  
+class Armstrong { 
+    public static boolean armstrong(int n){ 
+        int temp, digits=0, last=0, sum=0; 
+        temp = n; 
+        while(temp > 0){ 
+            temp = temp/10; 
+            digits++; 
+        } 
+        temp = n; 
+        while(temp > 0){ 
+            last = temp % 10; 
+            sum += (Math.pow(last, digits));   
+            temp = temp/10;
+        }
+        if(n==sum)   
+        return true; 
+        else return false;   
+    } 
+    public static void main(String args[]){     
+        int num;
+        Scanner sc= new Scanner(System.in);  
+        System.out.print("The limit is : "); 
+        num=sc.nextInt();  
+        System.out.println("Armstrong Number up to "+ num + " are: ");
+        for(int i=0; i<=num; i++) 
+        if(isArmstrong(i))
+        System.out.print(i+ ", ");
+    }   
+}
+class Palindrome{ 
+    public static boolean palindrome(int n){
+        public static void main(String[] args) {
+            String str = "Computer", reverseStr = "";
+            int strLength = str.length();
+            for (int i = (strLength - 1); i >=0; --i) {
+                reverseStr = reverseStr + str.charAt(i);
+    }
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+        System.out.println(str + " is not a Palindrome String.");
+    }
+        }
+    }
+}
+class Factorial{ 
+    public static int factorial(int n){
         
     }
-    class Palindrome{
-        
-    }
-    class Factorial{
-        
-    }
+}

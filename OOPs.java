d3f339fcb7e886e5b7517d8bff18d95861c8d3b5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-05-03 12:26:56 +0530

    Update OOPs.java

--- OOPs.java
+++ OOPs.java
@@ -112,8 +112,22 @@ public class TestGarbage1{
 }
 
 // Exceptions 
-// Try-Catch Exception Example
 
+// Try-Catch Exception Example
+class Exception{
+    public static void main(String args[]){
+        int a, b;
+        try{
+            b = 0;
+            a = 10 / b;
+            System.out.println("This will not be printed.");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Division by zero.");
+        }
+        System.out.println("After catch statement.");
+    }
+}
 
 
 

6dfdb7bc418ac333948fa6a13106bdae90b7c448
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-05-16 08:11:57 +0530

    Update OOPs.java

--- OOPs.java
+++ OOPs.java
@@ -285,6 +285,32 @@ class Throw{
 }
 
 
+
+
+
+
+
+// The Main Thread
+class CurrentThreadDemo{
+    public static void main(String args[]){
+        Thread t =Thread.currentThread();
+        System.out.println("Current Thread : " + t);
+        t.setName("My Thread");
+        System.out.println("After name change : " + t);
+        try{
+            for(int n = 5; n > 0; n--){
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Main thread interrupted");
+        }
+    }
+}
+
+
+
 // Thread creating
 class NewThread implements Runnable{
     Thread t;
@@ -355,6 +381,8 @@ class GenDemo{
         System.out.println();
     }
 }
+
+
 
 
 

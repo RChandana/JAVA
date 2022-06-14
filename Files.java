ff1301c74888c1f53075ba8ec058989cb5308077
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-15 00:37:56 +0530

    Update Files.java

--- Files.java
+++ Files.java
@@ -1,1 +1,23 @@
+// Creating a new File.
 
+import java.io.File;
+import java.io.IOException;
+class HelloWorld {
+    public static void main(String[] args) {
+        try{
+            File myObj = new File("File Path");
+            if(myObj.createNewFile()){
+                System.out.println("File Created : " + myObj.getName());
+            }
+            else{
+                System.out.println("File already exists.");
+            }
+        } 
+        catch(IOException e){
+            System.out.println("Error");
+        }
+    }
+}
+
+
+//

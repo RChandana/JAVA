a1533d05fbef9d60ae0bc45b1e1fe5ab20ed0ef2
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-07-25 20:23:25 +0530

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,1 +1,9 @@
-
+// Write a Java Program to diplay the Student Details from a text file in a HashMap and then store this information in an ArrayList.
+/*HashMap<String, String> 
+   Keys = Roll,
+          Name, 
+          Age,
+          Marks,
+          
+    ArrayList<HashMap> 
+    */

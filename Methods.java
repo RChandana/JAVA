5b74e0b4e8ed41f1a23c71a2b4dab900adf33083
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-06-16 21:56:09 +0530

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,10 +1,10 @@
-// A method in Java is a block of code that, when called, performs specific actions mentioned in it
-// You can insert values or parameters into methods, and they will only be executed when called. 
-// They are also referred to as functions. The primary uses of methods in Java are:
+/* A method in Java is a block of code that, when called, performs specific actions mentioned in it
+You can insert values or parameters into methods, and they will only be executed when called. 
+They are also referred to as functions. The primary uses of methods in Java are:
 
-        // It allows code reusability (define once and use multiple times).
-       // You can break a complex program into smaller chunks of code.
-      // It increases code readability.
+It allows code reusability (define once and use multiple times).
+You can break a complex program into smaller chunks of code.
+It increases code readability. */
 
 /* The six components for declaring the methods are:
    1) Access Specifiers
